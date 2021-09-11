85abf9cd324a2330df52341882e8af25ad6ba56e
Dawid33 <dawidsobczak@fastmail.com>
2021-09-11 19:11:07 +0100

    added frontend

---
+++ yew-frontend/target/debug/deps/yew_frontend-c3d083ba2304c49d.d
@@ -0,0 +1,5 @@
+/home/dawids/Projects/c++/website/yew-frontend/target/debug/deps/yew_frontend-c3d083ba2304c49d: src/main.rs
+
+/home/dawids/Projects/c++/website/yew-frontend/target/debug/deps/yew_frontend-c3d083ba2304c49d.d: src/main.rs
+
+src/main.rs:
---
+++ yew-frontend/target/debug/yew-frontend.d
@@ -0,0 +1,1 @@
+/home/dawids/Projects/c++/website/yew-frontend/target/debug/yew-frontend: /home/dawids/Projects/c++/website/yew-frontend/src/main.rs
